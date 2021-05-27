149df2f9f8312e63109b4c70cd585e74cd6f5b21
a.epik@mail.ru <u48zu1R7Tqx4>
2021-05-28 00:38:19 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Menu.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponentForm2()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 130);
+            this.button1.TabIndex = 0;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(buttonForm2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(129, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 130);
+            this.button2.TabIndex = 1;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(buttonForm2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        private void buttonForm2_Click(object sender, EventArgs e)
+        {
+            string s = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             Controls.Clear();
+                
+            InitializeComponentForm2();
+
+        }
+
+        private void Menu_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
