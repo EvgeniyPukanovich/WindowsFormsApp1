ecafb15bccce7f2784f7d169dfb4a1eb92d3e3db
a.epik@mail.ru <u48zu1R7Tqx4>
2021-06-01 20:16:22 +0500

    basiacally init commit

---
+++ WindowsFormsApp1/GameModel.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    enum CellState
+    {
+        O = -1,
+        Empty,
+        X
+    }
+
+    enum GameResult
+    {
+        IsGoing,
+        Draw,
+        Won
+    }
+
+    class GameModel
+    {
+        private CellState[] Field;
+        private CellState CurrentTurn;
+        private bool AgainstAI;
+        private Socket Socket;
+        public string IP = "127.0.0.1";
+        public int Port = 8080;
+        public int side = 4;
+
+        public GameModel(CellState firstTurn, bool againstAI)
+        {
+            CurrentTurn = firstTurn;
+            Field = new CellState[side*side];
+            AgainstAI = againstAI;
+        }
+
+        public async void MakeTurn(int position)
+        {
+            GameResult result = ChangeField(position);
+
+            if (AgainstAI)
+            {
+                if (Socket == null)
+                {
+                    IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(IP), Port);
+                    Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                    BeforeSendAction?.Invoke();
+
+                    try
+                    {
+                        await Task.Run(() => Socket.Connect(ipPoint));
+                    }
+                    catch (Exception ex)
+                    {
+                        ErrorOccured?.Invoke(ex.Message);
+                        return;
+                    }
+                    AfterSendAction?.Invoke();
+                    Socket.SendTimeout = 10000;
+                    Socket.ReceiveTimeout = 10000;
+                }
+
+                BeforeSendAction?.Invoke();
+
+                try
+                {
+                    await Task.Run(() => Socket.Send(new byte[] { (byte)position }));
+                }
+                catch (SocketException ex)
+                {
+                    ErrorOccured?.Invoke(ex.Message);
+                    return;
+                }
+
+                if (result == GameResult.IsGoing)
+                {
+                    byte[] data = new byte[256];
+
+                    try
+                    {
+                        await Task.Run(() => { int bytes = Socket.Receive(data, data.Length, 0); });
+                    }
+                    catch (SocketException ex)
+                    {
+                        ErrorOccured?.Invoke(ex.Message);
+                        return;
+                    }
+                    AfterSendAction?.Invoke();
+                    ChangeField(data[0]);
+                }
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (Socket != null)
+                Socket.Close();
+        }
+
+        private GameResult ChangeField(int position)
+        {
+            Field[position] = CurrentTurn;
+            MadeTurn?.Invoke(position, CurrentTurn);
+            CurrentTurn = CurrentTurn == CellState.O ? CellState.X : CellState.O;
+
+            (GameResult result, int[] wonPos) = CheckIfGameEnd();
+            if (result != GameResult.IsGoing)
+            {
+                CurrentTurn = CellState.X;
+                for (int i = 0; i < side*side; i++)
+                    Field[i] = CellState.Empty;
+                GameEnd?.Invoke(result, wonPos);
+
+            }
+            return result;
+        }
+
+        private (GameResult result, int[] wonPos) CheckIfGameEnd()
+        {
+            bool a = true;
+            CellState sign;
+            List<int> wonPos = new List<int>();
+            for (int i = 0; i < side; i++)//по вертикали
+            {
+                wonPos.Clear();
+                sign = Field[i];
+                if (sign == CellState.Empty)
+                    continue;
+                for (var j = 1; j < side; j++)
+                {
+                    wonPos.Add(i + j * side);
+                    if (Field[i+j*side] != sign)
+                    {
+                        a = false;
+                        break;
+                    }
+                }
+                if (a == true)
+                    return (GameResult.Won, wonPos.ToArray());
+            }
+            a = true;
+            for (int i = 0; i < side; i ++)//по горизонтали
+            {
+                wonPos.Clear();
+                sign = Field[i];
+                if (sign == CellState.Empty)
+                    continue;
+                for (var j = 1; j < side; j++)
+                {
+                    wonPos.Add(i * side + j);
+                    if (Field[i*side + j] != sign)
+                    {
+                        a = false;
+                        break;
+                    }
+                }
+                if (a == true)
+                    return (GameResult.Won, wonPos.ToArray());
+            }
+            a = true;
+            sign = Field[0];
+            wonPos.Clear();
+            if (sign != CellState.Empty)
+            {
+                for (int i = 0; i < side; i++)
+                {
+                    wonPos.Add(i + side * i);
+                    if (Field[i + side * i] != sign)
+                    {
+                        a = false;
+                        break;
+                    }
+                }
+                if (a == true)
+                    return (GameResult.Won, wonPos.ToArray());
+            }
+
+            wonPos.Clear();
+            if (sign != CellState.Empty)
+            {
+                for (int i = side - 1; i < side * side; i += side - 1)
+                {
+                    wonPos.Add(i + side * i);
+                    if (Field[i + side * i] != sign)
+                    {
+                        a = false;
+                        break;
+                    }
+                }
+                if (a == true)
+                    return (GameResult.Won, wonPos.ToArray());
+            }
+
+
+            foreach (var item in Field)
+            {
+                if (item == CellState.Empty)
+                    return (GameResult.IsGoing, null);
+            }
+
+            return (GameResult.Draw, null);
+        }
+
+        public event Action<int, CellState> MadeTurn;
+        public event Action<GameResult, int[]> GameEnd;
+        public event Action BeforeSendAction;
+        public event Action AfterSendAction;
+        public event Action<string> ErrorOccured;
+    }
+}
---
+++ WindowsFormsApp1/ModelView.cs
@@ -0,0 +1,231 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    class ModelView : Form
+    {
+
+        public ModelView()
+        {
+            CreateMenu();
+        }
+
+        private void CreateMenu()
+        {
+            Button button1 = new System.Windows.Forms.Button();
+            Button button2 = new System.Windows.Forms.Button();
+            TableLayoutPanel tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Anchor = (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right);
+            button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button1.Location = new System.Drawing.Point(128, 89);
+            button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            button1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(203, 83);
+            button1.TabIndex = 0;
+            button1.Text = "1v1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += (sender, args) =>
+            {
+                Controls.Clear();
+                CreateGame(new GameModel(CellState.X, false));
+            };
+            // 
+            // button2
+            // 
+            button2.Anchor = (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right);
+            button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button2.Location = new System.Drawing.Point(128, 263);
+            button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            button2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(203, 83);
+            button2.TabIndex = 1;
+            button2.Text = "vAI";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += (sender, args) =>
+            {
+                Controls.Clear();
+                CreateGame(new GameModel(CellState.X, true));
+            };
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right);
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.27273F));
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45.45454F));
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.27273F));
+            tableLayoutPanel1.Controls.Add(button1, 1, 1);
+            tableLayoutPanel1.Controls.Add(button2, 1, 3);
+            tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 5;
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            tableLayoutPanel1.Size = new System.Drawing.Size(460, 437);
+            tableLayoutPanel1.TabIndex = 2;
+            // 
+            // Menu
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(484, 461);
+            Controls.Add(tableLayoutPanel1);
+            Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            Name = "Menu";
+            Text = "Tic Tac Toe";
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+
+        }
+
+        private void CreateGame(GameModel model)
+        {
+            GameModel gameModel = model;
+            TableLayoutPanel table;
+
+            table = new TableLayoutPanel();
+
+            table.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+
+            for (int i = 0; i < gameModel.side; i++)
+            {
+                table.RowStyles.Add(new RowStyle(SizeType.Percent, 100f/gameModel.side));
+                table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100f /gameModel.side ));
+            }
+
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 7));
+            Button backButton = new Button();
+            backButton.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            backButton.Height = 30;
+            backButton.Margin = new Padding(3, 3, 30, 3);
+            backButton.Text = "Back";
+            backButton.Click += (sender, args) =>
+            {
+                gameModel.CloseConnection();
+                Controls.Clear();
+                CreateMenu();
+            };
+            table.Controls.Add(backButton, 0, gameModel.side);
+
+
+            table.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+
+            List<Button> buttons = new List<Button>();
+            bool[] disabledButtons = new bool[gameModel.side * gameModel.side];
+
+            for (int column = 0; column < gameModel.side; column++)
+            {
+                for (int row = 0; row < gameModel.side; row++)
+                {
+                    var iRow = row;
+                    var iColumn = column;
+                    var button = new Button();
+                    button.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+                    button.Dock = DockStyle.Fill;
+                    button.BackgroundImageLayout = ImageLayout.Zoom;
+                    button.FlatStyle = FlatStyle.Flat;
+                    button.Click += (sender, args) =>
+                    {
+                        gameModel.MakeTurn(iColumn * gameModel.side + iRow);
+                    };
+                    buttons.Add(button);
+                    table.Controls.Add(button, iColumn, iRow);
+                }
+            }
+
+            FormClosing += (sender, args) => gameModel.CloseConnection();
+
+            gameModel.BeforeSendAction += () =>
+            {
+                foreach (var item in buttons)
+                    item.Enabled = false;
+            };
+
+            gameModel.AfterSendAction += () =>
+            {
+                for (int i = 0; i < gameModel.side*gameModel.side; i++)
+                {
+                    if (!disabledButtons[i])
+                        buttons[i].Enabled = true;
+                }
+            };
+
+            gameModel.MadeTurn += (index, state) =>
+            {
+                Image cross = Properties.Resources.cross;
+                Image circle = Properties.Resources.circle;
+                buttons[index].BackgroundImage = state == CellState.O ? circle : cross;
+                buttons[index].Enabled = false;
+                disabledButtons[index] = true;
+            };
+
+            gameModel.GameEnd += (result, winPosition) =>
+            {
+                foreach (var item in buttons)
+                    item.Enabled = false;
+
+                Timer timer = new Timer();
+                timer.Interval = 2000;
+                timer.Tick += (sender, args) =>
+                {
+                    timer.Stop();
+                    foreach (var item in buttons)
+                    {
+                        item.BackColor = SystemColors.Control;
+                        item.BackgroundImage = null;
+                        item.Enabled = true;
+                    }
+                };
+
+                if (winPosition == null)
+                {
+                    foreach (var item in buttons)
+                    {
+                        //item.BackColor = Color.Yellow;
+                    }
+                }
+                else
+                {
+                    //foreach (var item in winPosition)
+                        //buttons[item].BackColor = Color.Green;
+                }
+                for (int i = 0; i < gameModel.side*gameModel.side; i++)
+                {
+                    disabledButtons[i] = false;
+                }
+                timer.Start();
+            };
+
+            gameModel.ErrorOccured += (message) =>
+            {
+                MessageBox.Show(message);
+                //Invoke((MethodInvoker)(() => { Controls.Clear(); CreateMenu(); }));
+                Controls.Clear();
+                CreateMenu();
+            };
+            table.Dock = DockStyle.Fill;
+            Controls.Add(table);
+        }
+    }
+}
